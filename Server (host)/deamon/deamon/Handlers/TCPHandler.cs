f78eb6f24bf11be011921f305e099c24282a5e97
Hugo Woesthuis <wijfjes@outlook.com>
2018-11-27 15:47:32 +0100

    update pc

---
+++ Server (host)/deamon/deamon/Handlers/TCPHandler.cs
@@ -0,0 +1,16 @@
+﻿/* Written by Hugo Woesthuis and Mike Krop
+ * Licensed under GPLv3
+ * See the LICENSE file for more info
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace deamon.Handlers
+{
+    class TCPHandler
+    {
+
+    }
+}
